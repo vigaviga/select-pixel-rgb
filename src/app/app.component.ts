1d0c8e63e4844df1908f2733993b464c5e08e965
Vigen Simonyan <vigen_simonyan@epam.com>
2023-10-24 16:44:06 +0400

    added pixel selection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+// app.component.ts
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-angular-app';
+  @ViewChild('imageCanvas', { static: true }) canvas: ElementRef<HTMLCanvasElement>;
+  private ctx: CanvasRenderingContext2D;
+
+  ngAfterViewInit() {
+    this.ctx = this.canvas.nativeElement.getContext('2d')!;
+  }
+
+  onFileSelected(event: Event) {
+    const files = (event.target as HTMLInputElement);
+    const file = files.files != null ? files.files[0] : null;
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = () => {
+        const img = new Image();
+        img.onload = () => {
+          this.ctx.clearRect(0, 0, this.canvas.nativeElement.width, this.canvas.nativeElement.height);
+          this.ctx.drawImage(img, 0, 0, this.canvas.nativeElement.width, this.canvas.nativeElement.height);
+        };
+        img.src = reader.result as string;
+      };
+      reader.readAsDataURL(file);
+    }
+  }
+
+  logPixelColor(event: MouseEvent) {
+    const x = event.offsetX;
+    const y = event.offsetY;
+
+    const pixel = this.ctx.getImageData(x, y, 1, 1).data;
+    const [r, g, b, a] = pixel; // RGBA values
+
+    console.log(`R: ${r}, G: ${g}, B: ${b}, A: ${a}`);
+  }
 }
